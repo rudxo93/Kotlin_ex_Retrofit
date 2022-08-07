36e5c1e4882b97b78d70a2b12d84e2f37ff04ef0
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-07 15:36:40 +0900

    Retrofit 간단한 예제를 통해 api통신 구현 Log로 값 찍어보기 -> 순차적으로 Log값이 찍히도록 코드 수정 -> 이렇게 하게 되면 콜백지옥(CallBack Hell)이 발생한다..

--- exRetrofit/app/src/main/java/com/duran/exretrofit/MainActivity.kt
+++ exRetrofit/app/src/main/java/com/duran/exretrofit/MainActivity.kt
@@ -17,15 +17,53 @@ https://jsonplaceholder.typicode.com/posts
 https://jsonplaceholder.typicode.com/posts/3
 */
 
+// 1 3 2 4  /  2 3 1 4 -> 현재 비동기적으로 실행되고 있어서 1 2 3 4 가 아닌 실행할때마다 무작위로 실행되었다.
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val api = RetrofitInstance.getInstnce().create(MyApi::class.java)
+
+        // 1. 유저 정보를 가져와서(userId)
         api.getPost1().enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
                 Log.e("API1", response.body().toString())
+                // 2. userId 기반으로 데이터를 가져오고 가져온 닉네임 값
+                api.getPostNumber(2).enqueue(object : Callback<Post> {
+                    override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                        Log.e("API2", response.body().toString())
+                        // 3. 닉네임 기반으로 데이터를 가져오고 유저의 댓글을 가져와서
+                        api.getPostNumber(3).enqueue(object : Callback<Post> {
+                            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                                Log.e("API3", response.body().toString())
+                                // 4. 유저의 댓글 기반으로 대댓글
+                                api.getPostNumber(4).enqueue(object : Callback<Post> {
+                                    override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                                        Log.e("API4", response.body().toString())
+                                    }
+
+                                    override fun onFailure(call: Call<Post>, t: Throwable) {
+                                        Log.e("API4", "fail")
+                                    }
+
+                                })
+                            }
+
+                            override fun onFailure(call: Call<Post>, t: Throwable) {
+                                Log.e("API3", "fail")
+                            }
+
+                        })
+                    }
+
+                    override fun onFailure(call: Call<Post>, t: Throwable) {
+                        Log.e("API2", "fail")
+                    }
+
+                })
             }
 
             override fun onFailure(call: Call<Post>, t: Throwable) {
@@ -34,15 +72,5 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        api.getPostNumber(2).enqueue(object : Callback<Post> {
-            override fun onResponse(call: Call<Post>, response: Response<Post>) {
-                Log.e("API2", response.body().toString())
-            }
-
-            override fun onFailure(call: Call<Post>, t: Throwable) {
-                Log.e("API2", "fail")
-            }
-
-        })
     }
 }
